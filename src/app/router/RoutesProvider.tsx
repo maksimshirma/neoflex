9e01d82b6db566a02349215eecec58dfbc583e03
Maksim Shirma <maksimshirma@mail.ru>
2025-04-18 19:09:28 +0300

    feat: add base routing

---
+++ src/app/router/RoutesProvider.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Outlet,
+  Route,
+} from 'react-router-dom';
+import { RoutesEnum, RoutesNamesMap } from '@/shared';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Outlet />}>
+      <Route path={RoutesEnum.Home} element={<>{RoutesNamesMap[RoutesEnum.Home]}</>} />
+      <Route path={RoutesEnum.Credits} element={<>{RoutesNamesMap[RoutesEnum.Credits]}</>} />
+      <Route path={RoutesEnum.Services} element={<>{RoutesNamesMap[RoutesEnum.Services]}</>} />
+      <Route index element={<Navigate to={RoutesEnum.Home} replace />} />
+    </Route>,
+  ),
+);
+
+export const RoutesProvider: FC = () => <RouterProvider router={router} />;
